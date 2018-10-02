ae444ea7010b5c6a1704488bdcda3c822807318d
Mike <sanders.michael.j@gmail.com>
2018-10-02 13:51:24 -0400

    added react logo to header

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,24 @@
+import "../index.css";
 import React from 'react';
 import List from "./list";
+import logo from "../logo.svg";
 import Form from "./form";
+
 import {observer} from "mobx-react";
 
 const App = observer(props => (
     <div>
         <div className="row mt-5 text-center">
-            <h1 className="w-100">Using: React & MobX State Tree</h1>
+            <div className="margin-auto">
+                <div className="d-inline-block">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                </div>
+                <div className="d-inline-block height-30">
+                    <div className="d-table-cell">
+                        <h1 className="w-100 align-middle d-inline-block">Using: React & MobX State Tree</h1>
+                    </div>
+                </div>
+            </div>
         </div>
         <div className="row mt-5">
             <div className="col-md-4 offset-md-1">
