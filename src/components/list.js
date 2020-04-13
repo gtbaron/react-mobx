555db8359897d60c633a4ab868423b05917a2b09
Mike Sanders <sanders.michael.j@gmail.com>
2020-04-13 08:55:52 -0400

    Changed component format

--- src/components/list.js
+++ src/components/list.js
@@ -1,17 +1,15 @@
 import React from "react";
 import {observer} from "mobx-react";
 
-const ConnectedList = ({articles}) => {
-    return (
-        <ul className="list-group list-group-flush">
-            { articles.map((el, index) => (
-                <li className="list-group-item" key={index}>
-                    {el.title}
-                </li>
-            ))}
-        </ul>
-    );
-};
+const ConnectedList = ({articles}) => (
+    <ul className="list-group list-group-flush">
+        { articles.map((el, index) => (
+            <li className="list-group-item" key={index}>
+                {el.title}
+            </li>
+        ))}
+    </ul>
+);
 
 const List = observer(ConnectedList);
 
